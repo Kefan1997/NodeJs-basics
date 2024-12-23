60d22c732ce6283db735ab5086c77278d3a57d32
Oleg Rashchinksy <oleg.rashchinksy@sharpgaming.com>
2024-12-23 17:15:01 +0100

    feat: implement create function

---
+++ src/fs/create.js
@@ -0,0 +1,29 @@
+import { writeFile } from 'node:fs';
+
+import { getPath, doesPathExist, operationFail } from '../helpers/index.js';
+
+const create = async () => {
+  const content = 'I am fresh and young';
+
+  const path = getPath(import.meta.url, 'files', 'fresh.txt');
+  const pathExists = await doesPathExist(path);
+
+  console.log('path', path)
+  console.log('pathExists', pathExists)
+
+  if (pathExists) {
+    operationFail();
+
+    return;
+  }
+
+  await writeFile(path, content, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('File created successfully');
+    }
+  });
+};
+
+await create();
