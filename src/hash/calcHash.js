2efd98b93965653b3678a62dd9e23314cea28b7b
Oleg Rashchinksy <oleg.rashchinksy@sharpgaming.com>
2024-12-24 10:44:24 +0100

    fix: update calcHash method

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,33 +1,39 @@
 import { createHash } from 'node:crypto';
 import { createReadStream } from 'node:fs';
 
-import { getPath, doesPathExist, operationFail } from '../helpers/index.js';
+import { getPath, operationFail } from '../helpers/index.js';
 
 const calcHash = async () => {
   const pathToFile = getPath(import.meta.url, 'files', 'fileToCalculateHashFor.txt');
-
-  if (!(await doesPathExist(pathToFile))) {
-    operationFail();
-    return;
-  }
 
   const hash = createHash('sha256');
   const stream = createReadStream(pathToFile);
 
   try {
-    stream.on('data', (data) => {
-      hash.update(data);
-    });
+    await new Promise((resolve, reject) => {
+      stream.on('data', (data) => {
+        hash.update(data);
+      });
 
-    stream.on('end', () => {
-      console.log('Hash:', hash.digest('hex'));
-    });
+      stream.on('end', () => {
+        console.log('Hash:', hash.digest('hex'));
+        resolve();
+      });
 
-    stream.on('error', (err) => {
-      throw err;
+      stream.on('error', (err) => {
+        reject(err);
+      });
     });
   } catch (err) {
-    throw err;
+    if (err.code === 'ENOENT') {
+      operationFail();
+      return;
+    } else if (err.code === 'EACCES') {
+      console.error('You do not have permission to read this file');
+      return;
+    } else {
+      console.error('An error occurred:', err.message);
+    }
   }
 };
 
