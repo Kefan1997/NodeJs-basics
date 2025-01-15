2a4452716e509465c7f331f30a2fb73893b50856
Oleg Rashchinksy <oleg.rashchinksy@sharpgaming.com>
2025-01-15 20:07:37 +0100

    feat: update read method

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,20 +1,19 @@
 import { readFile } from 'node:fs/promises';
 
-import { getPath, doesPathExist} from '../helpers/index.js';
+import { getPath } from '../helpers/index.js';
 
 const read = async () => {
   const pathToFile = getPath(import.meta.url, 'files', 'fileToRead.txt');
 
-  if (!(await doesPathExist(pathToFile))) {
-    throw new Error('FS operation failed');
-  }
-
   try {
     const content = await readFile(pathToFile, 'utf-8');
-    console.log('File content:', content); 
-  } catch(err) {
+    console.log('File content:', content);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
     throw err;
   }
-}
+};
 
 await read();
