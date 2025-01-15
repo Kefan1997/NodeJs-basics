6b11711539e256ada5aff11184401eebcf5dfd15
Oleg Rashchinksy <oleg.rashchinksy@sharpgaming.com>
2025-01-15 20:05:37 +0100

    feat: update list method

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,18 +1,17 @@
 import { readdir } from 'node:fs/promises';
 
-import { getPath, doesPathExist, } from '../helpers/index.js';
+import { getPath } from '../helpers/index.js';
 
 const list = async () => {
   const pathToFolder = getPath(import.meta.url, 'files');
-
-  if (!(await doesPathExist(pathToFolder))) {
-    throw new Error('FS operation failed');
-  }
 
   try {
     const files = await readdir(pathToFolder);
     console.log('Files in the folder:', files);
   } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
     throw err;
   }
 };
